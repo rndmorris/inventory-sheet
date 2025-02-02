5f1b66ca51941e07a34419b4b255efb8a5cde95d
rndmorris <13648552+rndmorris@users.noreply.github.com>
2025-02-02 04:22:09 -0600

    Configure astro for Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rndmorris.github.io',
+  base: 'inventory-sheet',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
