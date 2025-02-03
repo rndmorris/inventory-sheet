e69ef75c850908d078bc453b0f250b4c89b8bc10
rndmorris <13648552+rndmorris@users.noreply.github.com>
2025-02-03 05:50:09 +0000

    Change title, remove leftover contexts

--- src/components/IndexPage.tsx
+++ src/components/IndexPage.tsx
@@ -1,15 +1,9 @@
 'use client';
 import { InventoryTab } from "./tabs/InventoryTab";
-import React, { createContext, useState } from "react";
+import React, { useState } from "react";
 import { buttonPrimary, buttonPrimaryPressed } from "./styles";
 import ItemsTab from "./tabs/ItemsTab";
 import type { JSX } from "astro/jsx-runtime";
-
-export const OpenModalContext = createContext<
-    ((modal: React.ReactNode) => void) | undefined
->(undefined);
-
-export const ExitModalContext = createContext<(() => void) | undefined>(undefined);
 
 const tabs: { [key: string]: {label: string, generator: () => JSX.Element}; } = {
     items: {label: "Items", generator: () => <ItemsTab />},
@@ -29,27 +23,23 @@ export default function IndexPage() {
     };
     
     return (
-        <OpenModalContext.Provider value={undefined}>
-            <ExitModalContext.Provider  value={undefined}>
-                <div id="container" className="w-full h-full p-5 box-border">
-                    <header>
-                        <h1 className="text-3xl text-white pb-5">
-                            Character Inventory Manager
-                        </h1>
-                        <nav>
-                            {Object.keys(tabs).map((key) => (
-                                <label key={key} className={openTab === key ? buttonPrimaryPressed() : buttonPrimary()} >
-                                    {tabs[key].label}
-                                    <input type="radio" name="open-tab" className="hidden" onChange={changeTab} value={key} defaultChecked={openTab === key} />
-                                </label>
-                            ))}
-                        </nav>
-                    </header>
-                    <main className="w-full">
-                        {tabComponent}
-                    </main>
-                </div>
-            </ExitModalContext.Provider>
-        </OpenModalContext.Provider>
+        <div id="container" className="w-full h-full p-5 box-border">
+            <header>
+                <h1 className="text-3xl text-white pb-5">
+                    Character Inventory Manager
+                </h1>
+                <nav>
+                    {Object.keys(tabs).map((key) => (
+                        <label key={key} className={openTab === key ? buttonPrimaryPressed() : buttonPrimary()} >
+                            {tabs[key].label}
+                            <input type="radio" name="open-tab" className="hidden" onChange={changeTab} value={key} defaultChecked={openTab === key} />
+                        </label>
+                    ))}
+                </nav>
+            </header>
+            <main className="w-full">
+                {tabComponent}
+            </main>
+        </div>
     );
 }
